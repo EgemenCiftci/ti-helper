89535d66c0a7a235feda697b544e6160a5854533
Egemen Çiftci <egemenciftci@gmail.com>
2024-10-25 22:50:46 +0200

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-
-
 const routes: Routes = [
   { path: '', loadComponent: () => import('./components/wizard/wizard.component').then(m => m.WizardComponent) },
   { path: 'settings', loadComponent: () => import('./components/settings/settings.component').then(m => m.SettingsComponent) },
